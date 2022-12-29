0cc0d5071149cec7fdd566285d994aaeedba7a04
Avi Biton <93123067+avi-biton@users.noreply.github.com>
2022-12-29 11:23:33 +0000

    End of section 4

--- flow-control/switch/begin/main.go
+++ flow-control/switch/begin/main.go
@@ -7,14 +7,26 @@ import (
 )
 
 func main() {
-	os := runtime.GOOS
+	// os := runtime.GOOS
 
 	// refactor with switch statement
-	if os == "linux" || os == "darwin" || os == "unix" {
+	// if os == "linux" || os == "darwin" || os == "unix" {
+	// 	fmt.Println("*nix variant")
+	// } else if os == "windows" {
+	// 	fmt.Println("Windows")
+	// } else {
+	// 	fmt.Printf("%s.\n", os)
+	// }
+
+	os := runtime.GOOS; 
+	switch os{
+	case "linux", "darwin", "unix":
 		fmt.Println("*nix variant")
-	} else if os == "windows" {
+	case "windows":
 		fmt.Println("Windows")
-	} else {
+	default:
 		fmt.Printf("%s.\n", os)
 	}
+	
+
 }
